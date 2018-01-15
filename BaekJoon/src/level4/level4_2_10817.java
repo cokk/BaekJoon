9608b3512cdeb2f10d3df53273541fcc0e8b3cf5
Dakyung Ko <kdkyung57@gmail.com>
2018-01-16 02:12:04 +0900

    git test

--- BaekJoon/src/level4/level4_2_10817.java
+++ BaekJoon/src/level4/level4_2_10817.java
@@ -1,7 +1,7 @@
 //Baekjoon Online Judge : https://www.acmicpc.net
 //Question 10817 : 세 수    
 //세 정수 A, B, C가 주어진다. 이 때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. 
-//미
+//미완 2 
 
 package level4;
 import java.util.Scanner;
