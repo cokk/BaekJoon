304c9f69ec1c6bd1f969b5c5b499f332422df15d
Dakyung Ko <kdkyung57@gmail.com>
2018-01-20 06:45:20 +0900

    level4_2 풀이 완성

--- BaekJoon/src/level4/level4_2_10817.java
+++ BaekJoon/src/level4/level4_2_10817.java
@@ -1,7 +1,6 @@
 //Baekjoon Online Judge : https://www.acmicpc.net
 //Question 10817 : 세 수    
 //세 정수 A, B, C가 주어진다. 이 때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오. 
-//미완 2 
 
 package level4;
 import java.util.Scanner;
@@ -10,29 +9,29 @@ public class level4_2_10817 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] num = new int[3];
-		int Max = 0, secondMax = 0, count = 0;
-
-		for (int i = 0; i < num.length; i++) {
-			num[i] = sc.nextInt();
-			if(num[i] < 1 || num[i] > 100)
-				System.exit(0);
-		}
-
-		for (int i = 0; i < num.length; i++) {
-				if (num[i] >= Max)
-					Max = num[i];
-		}
-
-		for (int i = 0; i < num.length; i++) {
-			if (count < 2 && num[i] < Max && num[i] >= secondMax){
-				secondMax = num[i];
-				count++;
-			} 
-		}
-		if(count == 0)
-			secondMax = Max;
+		int A,B,C;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
 		
-		System.out.println(secondMax);
+		if(A > B){
+			if(A >= C){
+				if(B >= C)
+					System.out.println(B);
+				else
+					System.out.println(C);
+			}else if(A < C){
+				System.out.println(A);
+			}
+		}else if(B >= A){
+			if(B >= C){
+				if(A >= C)
+					System.out.println(A);
+				else
+					System.out.println(C);
+			}else if(B < C){
+				System.out.println(B);
+			}
+		}		
 	}
 }
