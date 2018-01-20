511ce694b55a7de381a3d507f55dc32afa08374f
Dakyung Ko <kdkyung57@gmail.com>
2018-01-20 13:08:02 +0900

    level5_2 풀이

--- BaekJoon/src/level5/Lv5_2_1065.java
+++ BaekJoon/src/level5/Lv5_2_1065.java
@@ -8,14 +8,30 @@ package level5;
 import java.util.Scanner;
 
 public class Lv5_2_1065 {
-	public static boolean arithSeq(int num){
-		boolean result = false;
-		return result;
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static int arithSeq(int num) {
 		int count=0;
 		
+		if( num < 100 ) { //1~99
+			return num; 
+		}
+			
+		for (int i = 100; i <= num; i++) { //100~999
+			int[] save = new int[3];
+			save[0] = i/100;
+			save[1] = (i%100)/10;
+			save[2] = (i%100)%10;
+			
+			if(save[1]-save[0] == save[2]-save[1])
+				count++;
+		}
+		return count+99;
 	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 0, input; // ~1000
+		input = sc.nextInt();
+
+		System.out.println(arithSeq(input));
+	}
 }
