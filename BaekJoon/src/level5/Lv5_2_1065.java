e7e921d509c169210beb319aaf410b19d6c9a368
Dakyung Ko <kdkyung57@gmail.com>
2018-01-20 11:40:27 +0900

    level5_1 풀이

---
+++ BaekJoon/src/level5/Lv5_2_1065.java
@@ -0,0 +1,21 @@
+//Baekjoon Online Judge : https://www.acmicpc.net
+//Question 1065 : 한수 
+//어떤 양의 정수 X의 자리수가 등차수열을 이룬다면, 그 수를 한수라고 한다. 
+//등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. 
+//N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+
+package level5;
+import java.util.Scanner;
+
+public class Lv5_2_1065 {
+	public static boolean arithSeq(int num){
+		boolean result = false;
+		return result;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count=0;
+		
+	}
+
+}
---
+++ BaekJoon/src/level5/Lv5_3_2448.java
@@ -0,0 +1,10 @@
+package level5;
+
+public class Lv5_3_2448 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
