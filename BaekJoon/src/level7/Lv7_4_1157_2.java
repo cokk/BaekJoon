83cc8f8f451fa11757508364432efa2b0fd129ac
Dakyung Ko <kdkyung57@gmail.com>
2018-01-28 16:56:31 +0900

    Lv7_4_1157 풀이

--- BaekJoon/src/level7/Lv7_4_1157_2.java
+++ BaekJoon/src/level7/Lv7_4_1157_2.java
@@ -1,7 +1,9 @@
 //Baekjoon Online Judge : https://www.acmicpc.net
 //Question 1157 : 단어 공부  
 //알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 
-//단, 대문자와 소문자를 구분하지 않는다. 그리고 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+//단, 대문자와 소문자를 구분하지 않는다. 
+//첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 
+//그리고 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 
 package level7;
 import java.util.Scanner;
@@ -16,7 +18,7 @@ public class Lv7_4_1157_2 {
 		for (int i = 0; i < input.length(); i++) {
 			if (input.charAt(i) > 'Z') {
 				apb[input.charAt(i) - 'a'] += 1;
-				if(apb[input.charAt(i)-'a'] == max)
+				if(apb[input.charAt(i) - 'a'] == max)
 					dup = max;
 				if(apb[input.charAt(i)-'a']>max){
 					max = apb[input.charAt(i)-'a'];
