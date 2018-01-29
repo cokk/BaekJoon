73d04ae69c566e8935fcf0c4ab32de78797877d2
Dakyung Ko <kdkyung57@gmail.com>
2018-01-29 23:56:40 +0900

    Lv1_1~1_6 풀이

---
+++ BaekJoon/src/level1/Lv1_2_1000.java
@@ -0,0 +1,15 @@
+//Baekjoon Online Judge : https://www.acmicpc.net
+//Question 1000 : A+B
+//두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+package level1;
+import java.util.Scanner;
+
+public class Lv1_2_1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(a+b);
+	}
+}
