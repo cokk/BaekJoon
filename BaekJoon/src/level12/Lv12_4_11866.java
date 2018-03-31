33888c3a002c22dc49d89246c8bdedabcccc6ad5
Dakyung Ko <kdkyung57@gmail.com>
2018-03-31 22:10:36 +0900

    변수명 수정

--- BaekJoon/src/level12/Lv12_4_11866.java
+++ BaekJoon/src/level12/Lv12_4_11866.java
@@ -1,19 +1,19 @@
 //Baekjoon Online Judge : https://www.acmicpc.net/problem/11866
 //Question 11866 : 조세퍼스 문제 0
 /*
-조세퍼스 문제는 다음과 같다.
-1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 M(≤ N)이 주어진다. 
-이제 순서대로 M번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
-이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, M)-조세퍼스 순열이라고 한다. 
-예를 들어 (7, 3)-조세퍼스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
-N과 M이 주어지면 (N,M)-조세퍼스 순열을 구하는 프로그램을 작성하시오.
- */
+ * 조세퍼스 문제는 다음과 같다.
+ * 1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 M(≤ N)이 주어진다. 
+ * 이제 순서대로 M번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 
+ * 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다. 원에서 사람들이 제거되는 순서를 (N, M)-조세퍼스 순열이라고 한다. 
+ * 예를 들어 (7, 3)-조세퍼스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+ * N과 M이 주어지면 (N,M)-조세퍼스 순열을 구하는 프로그램을 작성하시오.
+*/
 
 package level12;
 
 import java.util.Scanner;
 
-class LinkedList3_2{
+class LinkedList4{
 	private class Node{
 		private int data;
 		private Node next;
@@ -28,7 +28,7 @@ class LinkedList3_2{
 	private Node tail;
 	private int size;
 	
-	public LinkedList3_2(){
+	public LinkedList4(){
 		head = null;
 		tail = null;
 		size = 0;
@@ -83,7 +83,7 @@ class LinkedList3_2{
 
 public class Lv12_4_11866 {
 	public static void main(String[] args) {
-		LinkedList3_2 jp = new LinkedList3_2();
+		LinkedList4 jp = new LinkedList4();
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt(), save;
 		
