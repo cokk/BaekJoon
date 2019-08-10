da14727f6faa5c6b349720468eab88e814bb73fe
dakyung ko <kdkyung57@gmail.com>
2019-08-10 15:20:17 +0900

    chore: Add gitignore

--- baekjoon/cpp/baekjoon/Baekjoon_2557.cpp
+++ baekjoon/cpp/baekjoon/Baekjoon_2557.cpp
@@ -1,11 +1,12 @@
 //
-//  main.cpp
+//  Baekjoon_2557.cpp
 //  baekjoon
 //
 //  Created by KoDakyung on 10/08/2019.
 //  Copyright © 2019 KoDakyung. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
